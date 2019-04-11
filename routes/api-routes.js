6f43dd9c23884ceee36fa38c64c6485cabe3e317
Semi Western <46465000+abrahammtp@users.noreply.github.com>
2019-04-10 23:28:55 -0400

    Need to work on tournaments.js inside the public directory

---
+++ routes/api-routes.js
@@ -0,0 +1,16 @@
+var db = require("../models");
+
+module.exports = function(app) {
+    app.get("/", function(req, res) {
+
+    });
+
+    app.post("/api/tourneys", function(req, res) {
+
+    });
+
+    app.put("/api/tourneys/:id", function(req, res) {
+
+    });
+  
+}
